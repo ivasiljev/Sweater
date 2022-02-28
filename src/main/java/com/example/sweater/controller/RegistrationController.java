7e4a769b396bb8cbade08e9212f8bff05b296ff0
Ivan Vasiljev <ivasiljev@internet.ru>
2022-02-28 12:53:00 +0300

    Session

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -2,7 +2,6 @@ package com.example.sweater.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
